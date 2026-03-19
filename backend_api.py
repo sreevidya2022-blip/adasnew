7211d18186049f97bfff6d567191687f7c1d081b
sreevidya2022-blip <sreevidya.2022@vitstudent.ac.in>
2026-03-19 13:16:26 +0530

    Update backend_api.py

--- backend_api.py
+++ backend_api.py
@@ -328,7 +328,7 @@ def detection_summary():
         return jsonify({'error': str(e)}), 500
 
 with app.app_context():
-    db.create_all()
+    db.create_all(checkfirst=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
